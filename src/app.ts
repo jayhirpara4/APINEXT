4df095b47841fe543d0131a7a07957af06b50e00
Jay Hirpara <jayhirpara948@gmail.com>
2024-07-02 21:16:06 +0530

    add global errorhandler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import globalErrorHandlers from './middlewares/globalErrorHandlers';
 
 const app = express();
 
@@ -6,8 +7,14 @@ const app = express();
 //Http method : GET, POST, PUT, PATCH, DELETE
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.get('/', (req, res, next) => {
+    
     res.json({message: "This is for only testting purpose only and we can try out most....", value: " Here it's "});
     
 });
+
+// Global error handler
+// Middleware
+
+app.use(globalErrorHandlers);
 
 export default app;
