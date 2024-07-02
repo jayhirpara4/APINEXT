9495e3778db9c690c45634719f13a60016fd8a5e
Jay Hirpara <jayhirpara948@gmail.com>
2024-07-02 15:15:22 +0530

    Created data pach system file...

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ const startServer = async () => {
 
     await connectDB();
 
-    const port = config.prot || 3000;
+    const port = config.prot || 4000;
 
     app.listen(port, () => {
         console.log(`Listener on port: ${port}`);
